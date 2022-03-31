8ce5c3fc6206d7d40038b36da8250175b34a30b5
andreasgylche <andreasgylche@gmail.com>
2022-03-31 19:47:25 +0200

    added Photo component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import PhotoGrid from "./components/PhotoGrid";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <PhotoGrid />
     </div>
   );
 }
---
+++ src/components/PhotoGrid.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Photo from "./Photo";
+
+function PhotoGrid() {
+  return (
+    <div className="photo-grid">
+      <Photo />
+      <Photo />
+      <Photo />
+      <Photo />
+      <Photo />
+    </div>
+  );
+}
+
+export default PhotoGrid;
