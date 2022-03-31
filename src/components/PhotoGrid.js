9f98cfeba3845bedb5c02fe48bfb3652cbac097c
andreasgylche <andreasgylche@gmail.com>
2022-03-31 20:52:51 +0200

    firebase added and PhotoGrid connected to database

--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -1,14 +1,34 @@
-import React from "react";
-import Photo from "./Photo";
+import React, { useEffect, useState } from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebaseConfig";
 
 function PhotoGrid() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const imageRef = collection(db, "images");
+    const q = query(imageRef, orderBy("createdAt", "desc"));
+    onSnapshot(q, (snapshot) => {
+      const images = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setImages(images);
+      console.log(images);
+    });
+  }, []);
+
   return (
     <div className="photo-grid">
-      <Photo />
-      <Photo />
-      <Photo />
-      <Photo />
-      <Photo />
+      {images.length === 0 ? (
+        <p>No images in the database 😳</p>
+      ) : (
+        images.map((image) => (
+          <div className="photo" key={image.id}>
+            <img src={image.imageUrl} alt={image.imageName} />
+          </div>
+        ))
+      )}
     </div>
   );
 }
